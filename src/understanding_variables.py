a1d78cf835493ad7fbe314e8ca3942a9ac02f393
victor <2530325@upv.edu.mx>
2025-10-28 08:59:28 -0600

    feat:understanding_variablesC:\Users\escobar\Python_projects\test_project

---
+++ src/understanding_variables.py
@@ -0,0 +1,9 @@
+python_message = "Hola amigo python"
+print (python_message ,"charly")
+print (python_message ,"paulo coehloe")
+print (python_message ,"albertano")
+print (python_message ,"vitor")
+
+"""
+traceback:es un registro de donde el intreprete tuvo problemas al intentar ejecutar el codigo
+"""
