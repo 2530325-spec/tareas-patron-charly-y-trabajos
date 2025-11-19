43f4d31e872916abdb2e7f81f77f613e33af2f8d
victor <2530325@upv.edu.mx>
2025-11-19 08:46:05 -0600

    dictionaries

---
+++ src/understanding_dictonaries.py
@@ -0,0 +1,25 @@
+#simple dictionary
+alien_0 = {'color':'green','points':5}
+print(alien_0['color'])
+
+#the simpliest dictiomary
+alien_1 = {}
+alien_1 = {'color':'yellow'}
+print(alien_1['color'])
+print(alien_0['points'])
+
+# empty dictionary
+alien_2 = {}
+
+# modifying key-value pairs to a dictionary
+alien_2 = {'color':'yellow'}
+alien_2['color'] = 'blue'
+
+
+
+
+# adding new key-value pairs
+alien_2['x_position'] = 0
+alien_2['y_position'] = 25
+
+print (alien_2)
